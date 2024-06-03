4f3b1190f2b5ace418d463d0668ac983ec082a83
UndersenF <begintolife@mail.ru>
2024-06-03 20:00:59 +0300

    home work is done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("task1");
 
+        String firstName = "Ivanov";
+        String secondName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + secondName + " " + lastName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println("task2");
+
+        String capital = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + capital);
+
+        System.out.println("task3");
+
+        fullName = "Иванов Семён Семёныч";
+        String changedName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф.И.О. сотрудника - " + changedName );
     }
 }
